eb0ff2a3a4da981ddf1d167293de6d8eb99007a4
dblunk88 <39381389+dblunk88@users.noreply.github.com>
2021-03-16 14:27:46 -0400

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TelevisionStatic',
+    version='0.01',
+    packages=[''],
+    url='https://github.com/dblunk88/VisualSnowStaticGenerator',
+    license='GNU General Public License version 3',
+    author='David Blunk',
+    author_email='david@blunk.cc',
+    description='Experiment with different visual noise elements for people suffering with the Visual Snow Syndrome'
+)
